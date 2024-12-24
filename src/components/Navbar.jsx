7ac98efdf36a47c34bf7c9879ea7781ce15d38c1
Nikita Choudhary <nikita.choudhary3108@gmail.com>
2024-12-24 17:25:00 +0530

    chore(html): add Navlink to navbar component from react-router-dom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,26 +4,38 @@ import "../style/navbarStyle.css";
 import { CiLock } from "react-icons/ci";
 import { CiCircleChevRight } from "react-icons/ci";
 import { CiShoppingCart } from "react-icons/ci";
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div>
       <nav className='navbar'>
-          <ul class="nav-ul">
-            <li><input type="search" className="search" placeholder="Search..." /></li>
-            <li>Products</li>
-            <li>Reviews </li>
-            <li>Services</li>
-            <li>FAQ</li>
-            <li>Contact</li>
-            <li><Button1 label="Cart" icon={CiShoppingCart} iconProps={{ size: 25, style: { color: '#384047' } }}></Button1></li>
-            <li><Button1 label="Login" icon={CiLock} iconProps={{ size: 25, style: { color: '#384047' } }}></Button1></li>
-            <li><Button1 label="Sign Up" icon={CiCircleChevRight} iconProps={{ size: 25, style: { color: '#384047' } }}></Button1></li>
-           
-          </ul>
+        <div className="logo-container">
+          <img src="../assets/Logo.png" alt="Logo" className="navbar-logo" />
+        </div>
+        <ul className="nav-ul">
+          <li><input type="search" className="search" placeholder="Search..." /></li>
+          <li><NavLink to="../pages/Products.jsx">Products</NavLink></li>
+          <li><NavLink to="../pages/Reviews.jsx">Reviews</NavLink></li>
+          <li><NavLink to="../pages/Services.jsx">Services</NavLink></li>
+          <li><NavLink to="../pages/FAQ.jsx">FAQ</NavLink></li>
+          <li><NavLink to="../pages/Contact.jsx">Contact</NavLink></li>
+
+          <li>
+            <NavLink to="../pages/Cart.jsx"><Button1 label="Cart" icon={CiShoppingCart} iconProps={{ size: 25, style: { color: '#384047' } }}></Button1></NavLink>
+          </li>
+
+          <li>
+            <NavLink to="../pages/Login.jsx"><Button1 label="Login" icon={CiLock} iconProps={{ size: 25, style: { color: '#384047' } }}></Button1></NavLink>
+          </li>
+
+          <li>
+            <NavLink to="../pages/SignUp.jsx"><Button1 label="Sign Up" icon={CiCircleChevRight} iconProps={{ size: 25, style: { color: '#384047' } }}></Button1></NavLink>
+          </li>
+        </ul>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
